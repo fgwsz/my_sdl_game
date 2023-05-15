2d4c731cf14a96271ed724811db46605c05129b4
fgwsz <3191000458@qq.com>
2023-05-15 11:04:26 +0800

    first commit

---
+++ build/CMakeFiles/my_sdl_game.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_sdl_game.dir/src/main.c.obj"
+  "CMakeFiles/my_sdl_game.dir/src/main.c.obj.d"
+  "libmy_sdl_game.dll.a"
+  "my_sdl_game.exe"
+  "my_sdl_game.exe.manifest"
+  "my_sdl_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_sdl_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <SDL2/SDL.h>
+int main( int argc, char* argv[] )
+{
+    const int WIDTH = 640;
+    const int HEIGHT = 480;
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
